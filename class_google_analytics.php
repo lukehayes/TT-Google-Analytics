caf7380f609b9cd8d208f8843e5aac2eb5ee3649
lukehayes <lukepurplehayes@gmail.com>
2019-04-19 10:58:58 +0100

    Added method stubs

--- class_google_analytics.php
+++ class_google_analytics.php
@@ -12,6 +12,20 @@ if ( ! class_exists("TT_Google_Analytics")) {
 		
 		public function __construct() {
 		}
+
+		/**
+		 * Add the code to the footer of a theme
+		 */
+		public function addFooter() {
+			// TODO
+		}
+
+		/**
+		 * Create the plugin settings page
+		 */
+		public function addSettingsPage() {
+			// TODO
+		}
 	}
 }
 
