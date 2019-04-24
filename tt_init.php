1d7084e02b6e930404474879056ac33f109fee0e
lukehayes <lukepurplehayes@gmail.com>
2019-04-24 14:58:27 +0100

    Corrected namespace to TT_GA*

--- tt_init.php
+++ tt_init.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
- * Plugin Name: Themetacular Google Analytics 
+ * Plugin Name: Themetacular Google Analytics
  * Plugin URI: http://themetacular.com/plugins/tt-google-analytics
- * Description: A simple plugin to easily add Google Analytics to your theme. 
+ * Description: A simple plugin to easily add Google Analytics to your theme.
  * Version: 0.1
  * Author: Themetacular
  * Author URI: http://themetacular.com
@@ -11,18 +11,18 @@
 
 //-- Constants
 
-// Directory this plugin lives in 
-define('TT_AM_PLUGIN_PATH', plugin_dir_path( __FILE__ )  );
+// Directory this plugin lives in
+define('TT_GA_PLUGIN_PATH', plugin_dir_path( __FILE__ )  );
 
-// Assets directory for the plugin 
-define('TT_AM_ASSETS_DIR', TT_AM_PLUGIN_PATH . '/assets');
+// Assets directory for the plugin
+define('TT_GA_ASSETS_DIR', TT_GA_PLUGIN_PATH . '/assets');
 
-// Styles directory for the plugin 
-define('TT_AM_STYLES_DIR', TT_AM_ASSETS_DIR . '/styles');
+// Styles directory for the plugin
+define('TT_GA_STYLES_DIR', TT_GA_ASSETS_DIR . '/styles');
 
 
 //-- File Includes
-include_once TT_AM_PLUGIN_PATH . '/class_google_analytics.php';
+include_once TT_GA_PLUGIN_PATH . '/class_google_analytics.php';
 
 // Include plugin css styles
-include_once TT_AM_PLUGIN_PATH . '/tt_styles.php';
+include_once TT_GA_PLUGIN_PATH . '/tt_styles.php';
