ecb9abb4b1dd240dea50e3349c85ffec50d7a8ed
lukehayes <lukepurplehayes@gmail.com>
2019-04-24 15:32:29 +0100

    Changed order of functions

--- includes/tt-google-analytics.php
+++ includes/tt-google-analytics.php
@@ -3,14 +3,15 @@
  * Themetacular Google Analytics
  */
 
+// Crate a settings page for the plugin
+function tt_create_settings_page() {
+    // TODO
+}
+add_action('admin_menu', 'tt_create_settings_page');
+
 // Load the google analytics code into the footer of the theme
 function tt_load_google_analytics_footer() {
 	// TODO
 }
 add_action('wp_footer', 'tt_load_google_analytics_footer');
 
-// Crate a settings page for the plugin
-function tt_create_settings_page() {
-	// TODO
-}
-add_action('admin_menu', 'tt_create_settings_page');
