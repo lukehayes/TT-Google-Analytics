ad3f2abd991166712a41305fe66fe5d692a2a428
lukehayes <lukepurplehayes@gmail.com>
2019-04-24 15:39:03 +0100

    Added callback functions for settings page

--- includes/tt-google-analytics.php
+++ includes/tt-google-analytics.php
@@ -3,11 +3,21 @@
  * Themetacular Google Analytics
  */
 
+// Register a new menu page
+function tt_register_menu_page() {
+    // TODO
+    add_menu_page( "TT Google Analytics", "TT Google Analytics", "edit_posts", "tt-google-analytics", "tt_create_settings_page", $icon_url = '', $position = null );
+}
+add_action('admin_menu', 'tt_register_menu_page');
+
 // Crate a settings page for the plugin
 function tt_create_settings_page() {
-    // TODO
+    ?>
+    <div class="wrap">
+        <p>Hello</p>
+    </div>
+    <?php
 }
-add_action('admin_menu', 'tt_create_settings_page');
 
 // Load the google analytics code into the footer of the theme
 function tt_load_google_analytics_footer() {
