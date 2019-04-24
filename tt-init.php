772ab3d65b11b055fd24d28a709341f05995ff85
lukehayes <lukepurplehayes@gmail.com>
2019-04-24 15:39:28 +0100

    Corrected class name

--- tt-init.php
+++ tt-init.php
@@ -14,7 +14,7 @@
 define('TT_GA_PLUGIN_PATH', plugin_dir_path( __FILE__ )  );
 
 // Plugin include path
-define('TT_GA_INC_PATH', TT_GA_PLUGIN_PATH . '/includes' );
+define('TT_GA_INC_PATH', TT_GA_PLUGIN_PATH . 'includes' );
 
 // Assets directory for the plugin
 define('TT_GA_ASSETS_DIR', TT_GA_PLUGIN_PATH . '/assets');
@@ -24,7 +24,7 @@ define('TT_GA_STYLES_DIR', TT_GA_ASSETS_DIR . '/styles');
 
 
 //-- File Includes
-include_once TT_GA_INC_PATH . '/class-tt-google-analytics.php';
+include_once TT_GA_INC_PATH . '/tt-google-analytics.php';
 
 // Include plugin css styles
 include_once TT_GA_INC_PATH . '/tt-styles.php';
