7904e48f64a47d5cafdccec3e41ece314f298946
lukehayes <lukepurplehayes@gmail.com>
2019-04-25 13:26:28 +0100

    Switch to table layout for Wordpress styling

--- includes/tt-google-analytics.php
+++ includes/tt-google-analytics.php
@@ -30,9 +30,16 @@ function tt_create_settings_page() {
 
             <?php settings_fields( 'tt_ga_form_setting' ); ?>
 
-            <h4>Enter your Google Analytics code into the input box below and then hit save!</h4>
-            <label>Google Analytics Code: </label>
-            <input type="text" name="tt_ga_code" value="<?php echo esc_attr(get_option('tt_ga_code')); ?>">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Google Analytics Code:</th>
+                    <td>
+                        <input type="text" name="tt_ga_code" value="<?php echo esc_attr( get_option('tt_ga_code') ); ?>" />
+                        <br>
+                        <span class="description">Add your Google Analytics code into the input box and then hit save! Alternatively, if you don't have one yet then you can get one <a href="https://analytics.google.com/analytics/web/">here.</a> </span>
+                    </td>
+                </tr>
+            </table>
 
             <?php submit_button(); ?>
         </form>
